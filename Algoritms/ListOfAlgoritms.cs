44866c2842ecf6f5d0f61bc3a09a5a995da74c75
EvgeniyPolik <98385225+EvgeniyPolik@users.noreply.github.com>
2022-05-10 23:40:48 +0500

    Алгоритм простое число

---
+++ Algoritms/ListOfAlgoritms.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Algoritms
+{
+    public class ListOfAlgoritms
+    {
+        public static void PrimeNumber()
+        {
+            Console.WriteLine("Введите проверяемое число");
+            string numberStr = Console.ReadLine();
+            int numberRepit;
+            try
+            {
+                int numberInt = int.Parse(numberStr);
+                if (numberInt < 10)
+                {
+                    numberRepit = numberInt;
+                }
+                else
+                {
+                    numberRepit = (int) Math.Pow(numberInt, 0.5);
+                }
+                
+                bool prime = true;
+                for (int i = 2; i < numberRepit; i++)
+                {
+                    if (numberInt % i == 0)
+                    {
+                        prime = false;
+                    }
+                }
+
+                if (prime)
+                {
+                    Console.WriteLine("Введенное число " + numberStr + " - простое");
+                }
+                else
+                {
+                    Console.WriteLine("Введенное число " + numberStr + " - составное");
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Не верный ввод");
+            }
+        }
+    }
+}
---
+++ Algoritms/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Algoritms
+{
+    internal class Program
+    {
+        private static void SelAlgoritm(int selectedAlgoritm, string nameAlgoritm)
+        {
+            Console.WriteLine("Выбран алгоритм: " + nameAlgoritm);
+            if (selectedAlgoritm == 1)
+            {
+                ListOfAlgoritms.PrimeNumber();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Dictionary<int, string> catalogAlgoritms = new Dictionary<int, string>()
+            {
+                {1, "Простое число"}
+            };
+
+            while (true)
+            {
+                Console.WriteLine("Выберите алгоритм:");
+                for (int i = 1; i < catalogAlgoritms.Count + 1; i++)
+                {
+                    Console.WriteLine(i.ToString() + " " + catalogAlgoritms[i]);
+                }
+                Console.WriteLine("Введите номер алгоритма или exit для выхода: ");
+                string inputStr = Console.ReadLine();
+                Debug.Assert(inputStr != null, nameof(inputStr) + " != null");
+                if (inputStr.ToLower() == "exit")
+                {
+                    return;
+                }
+
+                try
+                {
+                    int numberOfAlgoritm = int.Parse(inputStr);
+                    SelAlgoritm(numberOfAlgoritm, catalogAlgoritms[numberOfAlgoritm]);
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Не верный формат ввода");
+                }
+                catch (System.Collections.Generic.KeyNotFoundException)
+                {
+                    Console.WriteLine("Не верный выбор");
+                }
+                    
+            }
+            
+        }
+    }
+}
