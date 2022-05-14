49ba772bef6bdf566e12deff1af1746eaad4318b
EvgeniyPolik <98385225+EvgeniyPolik@users.noreply.github.com>
2022-05-14 23:14:58 +0500

    Линейный поиск

--- Algoritms/ListOfAlgoritms.cs
+++ Algoritms/ListOfAlgoritms.cs
@@ -4,7 +4,7 @@ namespace Algoritms
 {
     public class ListOfAlgoritms
     {
-        public static void PrimeNumber()
+        public static void PrimeNumber()  // Проверка простого числа
         {
             Console.WriteLine("Введите проверяемое число");
             string numberStr = Console.ReadLine();
@@ -43,6 +43,51 @@ namespace Algoritms
             {
                 Console.WriteLine("Не верный ввод");
             }
+        }
+
+        public static void LinearSearch()
+        {
+            bool doing = true;
+            while (doing = true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите размер массива: ");
+                    string countString = Console.ReadLine();
+                    if (countString == "stop")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int count = int.Parse(countString);
+                        Console.WriteLine("Введите искомый элемент:");
+                        string foundNum = Console.ReadLine();
+                        string[] mass = new string[count];
+                        string answer = "Not Found";
+                        for (int i = 0; i < count; i++)
+                        {
+                            Console.Write($"Вводите элемент массива № {i + 1}: ");
+                            mass[i] = Console.ReadLine();
+                        }
+                        for (int j = 0; j < count; j++)
+                        {
+                            if (mass[j] == foundNum)  // Собственно сам поиск(упрощенно) проверяем все элементы массива
+                            {
+                                answer = (j+1).ToString();
+                            }
+                        }
+                        Console.WriteLine("Искомый элемент находится на позиции:" + answer);
+                        break;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Неверный ввод, для выхода введите stop");
+                }
+            }
+ 
+            
         }
     }
 }
--- Algoritms/Program.cs
+++ Algoritms/Program.cs
@@ -6,24 +6,29 @@ namespace Algoritms
 {
     internal class Program
     {
-        private static void SelAlgoritm(int selectedAlgoritm, string nameAlgoritm)
+        private static void SelAlgoritm(int selectedAlgoritm, string nameAlgoritm)  //Метод вызова выбранного алгоритма
         {
             Console.WriteLine("Выбран алгоритм: " + nameAlgoritm);
             if (selectedAlgoritm == 1)
             {
                 ListOfAlgoritms.PrimeNumber();
             }
+            else if (selectedAlgoritm == 2)
+            {
+                ListOfAlgoritms.LinearSearch();   
+            }
         }
 
         public static void Main(string[] args)
         {
 
-            Dictionary<int, string> catalogAlgoritms = new Dictionary<int, string>()
+            Dictionary<int, string> catalogAlgoritms = new Dictionary<int, string>() //Словарь алгоритмов
             {
-                {1, "Простое число"}
+                {1, "Простое число"},
+                {2, "Линейный поиск"}
             };
 
-            while (true)
+            while (true)  // Интрефейс выбора алгоритма
             {
                 Console.WriteLine("Выберите алгоритм:");
                 for (int i = 1; i < catalogAlgoritms.Count + 1; i++)
@@ -35,21 +40,21 @@ namespace Algoritms
                 Debug.Assert(inputStr != null, nameof(inputStr) + " != null");
                 if (inputStr.ToLower() == "exit")
                 {
-                    return;
+                    return;  // Завершаем метод, выходим из программы
                 }
 
-                try
+                try  // проверка корректности ввода
                 {
                     int numberOfAlgoritm = int.Parse(inputStr);
                     SelAlgoritm(numberOfAlgoritm, catalogAlgoritms[numberOfAlgoritm]);
                 }
                 catch (System.FormatException)
                 {
-                    Console.WriteLine("Не верный формат ввода");
+                    Console.WriteLine("Неверный формат ввода");
                 }
                 catch (System.Collections.Generic.KeyNotFoundException)
                 {
-                    Console.WriteLine("Не верный выбор");
+                    Console.WriteLine("Неверный выбор");
                 }
                     
             }
